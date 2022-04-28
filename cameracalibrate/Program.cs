c61e942e42bed583151360cb8988b9bc08dee146
秋田拓海 <65523539+TakumiAkita@users.noreply.github.com>
2022-04-28 16:15:12 +0900

    0428学校作業

--- cameracalibrate/Program.cs
+++ cameracalibrate/Program.cs
@@ -1,2 +1,14 @@
-﻿using OpenCvSharp;
+﻿using System;
+using OpenCvSharp;
 
+namespace CameraCalibration
+{
+    class Program
+    {
+        public void Run()
+        {
+            var imgPath
+            Cv2.ImRead();
+        }
+    }
+}
