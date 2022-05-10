940629a74e9e0f4dd6e1afc7a4c8fd4eeb3f5276
秋田拓海 <65523539+TakumiAkita@users.noreply.github.com>
2022-05-10 17:58:50 +0900

    0510学校作業

--- cameracalibrate/Program.cs
+++ cameracalibrate/Program.cs
@@ -11,8 +11,16 @@ namespace CameraCalibration
             var img = new Mat(); 
             img = Cv2.ImRead(imgPath);
             var criteria = new TermCriteria(CriteriaTypes.Eps, 30, 0.001);
+            var gray = new Mat();
+            Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+            var corners= new Mat();
+            var corner = new Point2f();
+            var flags = new ChessboardFlags();
+            Cv2.FindChessboardCorners(gray, new Size(6, 6), corners,flags);
+            if(flags)
+            {
 
-
+            }
         }
     }
 }
