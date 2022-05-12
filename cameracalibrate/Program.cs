df812ad699edfcb793ade5d0fa67b488a0dba5a9
秋田拓海 <65523539+TakumiAkita@users.noreply.github.com>
2022-05-12 16:31:11 +0900

    0512学校作業 mainが実行できない！！

--- cameracalibrate/Program.cs
+++ cameracalibrate/Program.cs
@@ -5,7 +5,7 @@ namespace CameraCalibration
 {
     class Program
     {
-        public void Run()
+        public void Main()
         {
             var imgPath= @"D:\\picture\\goprochessbord\\c300421";
             var img = new Mat(); 
@@ -14,12 +14,18 @@ namespace CameraCalibration
             var gray = new Mat();
             Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
             var corners= new Mat();
-            var corner = new Point2f();
+            
             var flags = new ChessboardFlags();
             var isfind = Cv2.FindChessboardCorners(gray, new Size(6, 6), corners,flags);
             if(isfind)
             {
-
+                Cv2.DrawChessboardCorners(gray, new Size(6, 6), corners, isfind);
+                Cv2.ImShow("sumple", gray);
+                Cv2.WaitKey(1);
+            }
+            else
+            {
+                Console.WriteLine("failed to find corners");
             }
         }
     }
