4d281a4ac6b34360f29e1d3324a0c1a50a029422
秋田拓海 <65523539+TakumiAkita@users.noreply.github.com>
2022-05-16 18:26:51 +0900

    0516学校作業

--- cameracalibrate/Program.cs
+++ cameracalibrate/Program.cs
@@ -5,28 +5,30 @@ namespace CameraCalibration
 {
     class Program
     {
-        public void Main()
+        public static void Main()
         {
-            var imgPath= @"D:\\picture\\goprochessbord\\c300421";
-            var img = new Mat(); 
-            img = Cv2.ImRead(imgPath);
+            var imgPath= @"D:\picture\chessboardresize\c101681.jpg";
+            var img = Cv2.ImRead(imgPath);
+            Cv2.ImShow("sumple", img);
+            
             var criteria = new TermCriteria(CriteriaTypes.Eps, 30, 0.001);
             var gray = new Mat();
             Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
             var corners= new Mat();
             
             var flags = new ChessboardFlags();
-            var isfind = Cv2.FindChessboardCorners(gray, new Size(6, 6), corners,flags);
+            //キャリブレーションボードに合わせてちゃんとサイズ(交差点？の数)を合わせよう！！ピッタリじゃないと検出できない
+            var isfind = Cv2.FindChessboardCorners(gray, new Size(7, 10), corners,flags);
             if(isfind)
             {
                 Cv2.DrawChessboardCorners(gray, new Size(6, 6), corners, isfind);
-                Cv2.ImShow("sumple", gray);
-                Cv2.WaitKey(1);
+                Cv2.ImShow("output", gray);
             }
             else
             {
                 Console.WriteLine("failed to find corners");
             }
+            Cv2.WaitKey(0);
         }
     }
 }
