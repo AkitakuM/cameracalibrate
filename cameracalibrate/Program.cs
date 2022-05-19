7856764a650e4b3a5e27df1d6c7eba99b68ca818
秋田拓海 <65523539+TakumiAkita@users.noreply.github.com>
2022-05-19 16:03:56 +0900

    ブランチ作成

--- cameracalibrate/Program.cs
+++ cameracalibrate/Program.cs
@@ -7,35 +7,70 @@ namespace CameraCalibration
     {
         public static void Main()
         {
-            var imgPath= @"D:\picture\chessboardresize\c101681.jpg";
-            var img = Cv2.ImRead(imgPath);
-            Cv2.ImShow("sumple", img);
-            
-            
-            var gray = new Mat();
-            Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
-            var corner= new Mat();
+            Program program = new();
+            bool isExistImg = true;
+            int i = 1;
             var corners = new List<Mat>();
-            
-            var flags = new ChessboardFlags();
-            //キャリブレーションボードに合わせてちゃんとサイズ(交差点？の数)を合わせよう！！ピッタリじゃないと検出できない
-            var isfind = Cv2.FindChessboardCorners(gray, new Size(7, 10), corner,flags);
-            if(isfind)
+            while (isExistImg)
             {
-                Cv2.DrawChessboardCorners(gray, new Size(7, 10), corner, isfind);
-                corners.Add(corner);
-                Cv2.ImShow("output", gray);
+                var imgPath = program.MakeImagePath(i);
+                var img = Cv2.ImRead(imgPath);
+                Cv2.ImShow("sumple", img);
+                if (img == null) isExistImg = false;
+               
+                        
+                var gray = new Mat();
+                Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+                var corner = new Mat();
+                
+
+                var flags = new ChessboardFlags();
+                //キャリブレーションボードに合わせてちゃんとサイズ(交差点？の数)を合わせよう！！ピッタリじゃないと検出できない
+                var isfind = Cv2.FindChessboardCorners(gray, new Size(7, 10), corner, flags);
+                if (isfind)
+                {
+                    Cv2.DrawChessboardCorners(gray, new Size(7, 10), corner, isfind);
+                    corners.Add(corner);
+                    Cv2.ImShow("output", gray);
+                    Console.WriteLine("{0} succeed to find corners", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} failed to find corners",i);
+                }
+                i++;
+            }
+        }
+
+        public string MakeImagePath(int num)
+        {
+            var a = num / 100;
+            var b = num / 10;
+            var c = num % 10;
+            var imgname = "cb" + a + b + c+".jpg";
+            var imgpath = @"D:\picture\chessboardresize\" + imgname;
+            return imgpath;
+        }
+
+        public Mat findcorner(Size size, Mat img)
+        {
+            var corner = new Mat();
+            var flags = new ChessboardFlags();
+            
+            var isfind = Cv2.FindChessboardCorners(img, size, corner, flags);
+            if (isfind)
+            {
+                Cv2.DrawChessboardCorners(img, size, corner, isfind);
+                Cv2.ImShow("output", img);
             }
             else
             {
                 Console.WriteLine("failed to find corners");
             }
             Cv2.WaitKey(0);
-        }
-
-        public string MakeImagePath()
-        {
+            return corner;
 
         }
     }
+
 }
